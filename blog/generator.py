1cb8fca05083f67050f1eb2f7cb0ac7d6a200f0c
Areeba Aziz <aziza11@mcmaster.ca>
2020-04-29 02:26:14 -0400

    finished blog generator script

--- blog/generator.py
+++ blog/generator.py
@@ -4,8 +4,11 @@ python generator.py <filename>
 
 import sys
 import re
+import os
 
 SECTIONS = "sections"
+TAGS = "tags"
+POSTS = "posts"
 
 try:
     filename = sys.argv[1]
@@ -30,6 +33,7 @@ text = file.read().strip()
 file.close()
 
 sections = re.findall("%% ?begin ?(\[[^\]]*][^%]*)%% ?end", text, re.DOTALL)
+full_content = ""
 
 for si in range(len(sections)):
     tags = re.findall("\[(.*)\]", sections[si])
@@ -38,6 +42,25 @@ for si in range(len(sections)):
     # create the section file
     text = re.findall("\[.*\](.*)", sections[si], re.DOTALL)[0].strip()
     if text != "": 
+        full_content += text
+
+        # write contents into the section file
         file = open(sec_filename, "w")
         file.write(text)
         file.close()
+
+        # create or append to tag files
+        for tag in tags:
+            file = open(TAGS + "/" + tag, "a")
+            file.write(sec_filename.replace(SECTIONS + "/", "") + "\n")
+            file.close()
+
+# now write the full contents of the blog post in one file
+blog_filename = POSTS + "/w-{week}".format(week=week)
+
+file = open(blog_filename + ".md", "w")
+file.write(full_content)
+file.close()
+
+# now write it in HTML format
+os.system("markdown2 {0}.md > {0}.html".format(blog_filename))
